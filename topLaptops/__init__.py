45aef60749ae74b1b217b2a29b0937d590d06667
Anant Kamath <kamathanant@gmail.com>
2016-10-28 06:16:48 +0530

    Dockerized the app

--- topLaptops/__init__.py
+++ topLaptops/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+import os
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
+app.config['MONGO_HOST'] = os.environ['DB_PORT_27017_TCP_ADDR']
 mongo = PyMongo(app)
 
 import topLaptops.views
