748b593c93cc3d3d459d30f515284860767cb311
Anant Kamath <kamathanant@gmail.com>
2016-10-30 19:18:03 +0530

    Set mongo host only if running in docker

--- topLaptops/__init__.py
+++ topLaptops/__init__.py
@@ -4,7 +4,11 @@ import os
 
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
-app.config['MONGO_HOST'] = os.environ['DB_PORT_27017_TCP_ADDR']
+
+# if running in docker, get the hostname from env var:
+if 'DB_PORT_27017_TCP_ADDR' in os.environ:
+    app.config['MONGO_HOST'] = os.environ['DB_PORT_27017_TCP_ADDR']
+
 mongo = PyMongo(app)
 
 import topLaptops.views
